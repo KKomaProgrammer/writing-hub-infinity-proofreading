5a54d15fa97f8e6ecf5ea16719cc9a78488eeddb
KKoma Programmer <ijunu3343@gmail.com>
2025-12-05 22:04:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ async function doProofread() {
         }
     };
 
-    const res = await fetch("/api/proofread", {
+    const res = await fetch("/api/proofreading", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
